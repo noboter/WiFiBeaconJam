8fe0eec07beb08a8b413567b378e3818c4630be0
Norbert <noboter@users.noreply.github.com>
2017-12-22 19:55:51 +0100

    Update WiFiBeaconJam.ino

--- WiFiBeaconJam.ino
+++ WiFiBeaconJam.ino
@@ -9,10 +9,6 @@
 #include "ouis.h"
 #include <ESP8266WiFi.h>
 
-extern "C" {
-  #include "user_interface.h"
-}
-
 String characters = "1234567890qwertyuiopasdfghjkklzxcvbnm QWERTYUIOPASDFGHJKLZXCVBNM_";
 byte channel;
 
